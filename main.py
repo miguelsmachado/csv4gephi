8c8e5d3dc2048cded8175bfe2444ec6ec77b4e41
Miguel S Machado <miguelsmachado@gmail.com>
2020-06-19 20:24:46 -0300

    versao inicial

--- main.py
+++ main.py
@@ -16,21 +16,24 @@ def get_table_download_link(df):
 def main():
     st.title("CSV 4 Gephi")
     st.markdown("Faça o upload do seu arquivo csv para que os dados sejam preparados para serem utlizados pelo Gephi.")
-    st.markdown("Caso tenha alguma dúvida de como gerar esse arquivo, consulte o tutorial "
+    st.markdown("Caso tenha alguma dúvida de como gerar esse arquivo, consulte o tutorial do "
                 "[Projeto](https://bit.ly/3dy0AQ1)")
-    st.header("Upload do arquivo")
+    st.markdown("___")
+    st.header("**Atenção**")
     st.subheader("Colunas mínimas")
     st.text("O arquivo baixado do Facepager, deve conter ao menos as seguintes colunas:")
     min_col = pd.DataFrame({'colunas':['user.screen_name', 'entities.user_mentions.0.screen_name', 'object_id',
                  'created_at', 'text', 'retweeted_status.retweet_count', 'user.location']})
     st.dataframe(min_col.colunas)
 
+    st.subheader("Separador")
     radio = st.radio("Antes de fazer o upload, escolha o separador:", ("Ponto-e-vírgula ( ; )", "Vírgula ( , )"))
     if radio == "Ponto-e-vírgula ( ; )":
         separador = ';'
     if radio == "Vírgula ( , )":
         separador = ','
 
+    st.header("Upload do arquivo")
     file = st.file_uploader('Escolha a base de dados que deseja preparar (.csv)', type='csv')
     if file is not None:
         df = pd.read_csv(file, sep=separador)
@@ -61,6 +64,7 @@ def main():
         number = st.slider('Escolha o numero de linhas que deseja visualizar:', min_value=1, max_value=100, value=10)
         st.dataframe(df.head(number))
 
+        st.markdown("___")
         st.header("Otimizando os parâmetros de busca")
 
         df_research = df[['target', 'tweet_text']]
@@ -88,6 +92,7 @@ def main():
         number1 = st.slider('Escolha o numero de linhas que deseja visualizar:', min_value=1, max_value=100, value=20)
         st.dataframe(top_words.head(number1))
 
+        st.markdown("___")
         st.header("Top Hashtags")
 
         # criando as novas variáveis que serão utilizadas
@@ -112,7 +117,7 @@ def main():
         number2 = st.slider('Selecione o número de #:', min_value=1, max_value=50, value=10)
         st.dataframe(top_hashtags.head(number2))
 
-        bt_wordcloud_hashtags = st.button("Gerar Wordcloud de #")
+        bt_wordcloud_hashtags = st.button("Gerar Wordcloud", key='hashtags')
         if bt_wordcloud_hashtags:
             # criando um texto com todas as hashtags
             all_hashtags = " ".join(s for s in hashtags)
@@ -125,11 +130,12 @@ def main():
             plt.imshow(wordcloud)
             st.pyplot()
 
-        #st.header("Top Mentions")
+        st.markdown("___")
+        st.header("Top Mentions")
         number3 = st.slider('Escolha o número de @:', min_value=1, max_value=50, value=10)
         st.dataframe(top_mentions.head(number3))
 
-        bt_wordcloud_hashtags = st.button("Gerar Wordcloud de @")
+        bt_wordcloud_hashtags = st.button("Gerar Wordcloud")
         if bt_wordcloud_hashtags:
             # criando um texto com todas as menções
             all_mentions = " ".join(s for s in mentions)
@@ -142,6 +148,7 @@ def main():
             plt.imshow(wordcloud)
             st.pyplot()
 
+        st.markdown("___")
         st.header("Maiores Influenciadores")
         st.subheader("São os usuários que foram mais retwitados")
 
@@ -162,6 +169,7 @@ def main():
                 "TOP INFLUENCIADORES")
             st.pyplot()
 
+        st.markdown("___")
         st.header("Maiores Twittadores")
         st.subheader("São os usuários que postaram sobre o assunto")
 
@@ -180,164 +188,166 @@ def main():
                 "TOP TWITTADORES")
             st.pyplot()
 
+        st.markdown("___")
         st.header("Encontrando a localização dos usuários:")
-        bt_loc_users = st.button("Encontrar usuários")
-        if bt_loc_users:
 
-            df_loc = pd.read_csv("municipios.csv")
-            df_municipios = df_loc[['nome', 'codigo_uf']]
-            df_user_loc = df['user.location']
+        df_loc = pd.read_csv("municipios.csv")
+        df_municipios = df_loc[['nome', 'codigo_uf']]
+        df_user_loc = df['user.location']
 
-            ro = df_municipios.query("codigo_uf==11")['nome'].str.lower()
-            ac = df_municipios.query("codigo_uf==12")['nome'].str.lower()
-            am = df_municipios.query("codigo_uf==13")['nome'].str.lower()
-            rr = df_municipios.query("codigo_uf==14")['nome'].str.lower()
-            pa = df_municipios.query("codigo_uf==15")['nome'].str.lower()
-            ap = df_municipios.query("codigo_uf==16")['nome'].str.lower()
-            to = df_municipios.query("codigo_uf==17")['nome'].str.lower()
-            ma = df_municipios.query("codigo_uf==21")['nome'].str.lower()
-            pi = df_municipios.query("codigo_uf==22")['nome'].str.lower()
-            ce = df_municipios.query("codigo_uf==23")['nome'].str.lower()
-            rn = df_municipios.query("codigo_uf==24")['nome'].str.lower()
-            pb = df_municipios.query("codigo_uf==25")['nome'].str.lower()
-            pe = df_municipios.query("codigo_uf==26")['nome'].str.lower()
-            al = df_municipios.query("codigo_uf==27")['nome'].str.lower()
-            se = df_municipios.query("codigo_uf==28")['nome'].str.lower()
-            ba = df_municipios.query("codigo_uf==29")['nome'].str.lower()
-            mg = df_municipios.query("codigo_uf==31")['nome'].str.lower()
-            es = df_municipios.query("codigo_uf==32")['nome'].str.lower()
-            rj = df_municipios.query("codigo_uf==33")['nome'].str.lower()
-            sp = df_municipios.query("codigo_uf==35")['nome'].str.lower()
-            pr = df_municipios.query("codigo_uf==41")['nome'].str.lower()
-            sc = df_municipios.query("codigo_uf==42")['nome'].str.lower()
-            rs = df_municipios.query("codigo_uf==43")['nome'].str.lower()
-            ms = df_municipios.query("codigo_uf==50")['nome'].str.lower()
-            mt = df_municipios.query("codigo_uf==51")['nome'].str.lower()
-            go = df_municipios.query("codigo_uf==52")['nome'].str.lower()
-            df1 = df_municipios.query("codigo_uf==53")['nome'].str.lower()
+        ro = df_municipios.query("codigo_uf==11")['nome'].str.lower()
+        ac = df_municipios.query("codigo_uf==12")['nome'].str.lower()
+        am = df_municipios.query("codigo_uf==13")['nome'].str.lower()
+        rr = df_municipios.query("codigo_uf==14")['nome'].str.lower()
+        pa = df_municipios.query("codigo_uf==15")['nome'].str.lower()
+        ap = df_municipios.query("codigo_uf==16")['nome'].str.lower()
+        to = df_municipios.query("codigo_uf==17")['nome'].str.lower()
+        ma = df_municipios.query("codigo_uf==21")['nome'].str.lower()
+        pi = df_municipios.query("codigo_uf==22")['nome'].str.lower()
+        ce = df_municipios.query("codigo_uf==23")['nome'].str.lower()
+        rn = df_municipios.query("codigo_uf==24")['nome'].str.lower()
+        pb = df_municipios.query("codigo_uf==25")['nome'].str.lower()
+        pe = df_municipios.query("codigo_uf==26")['nome'].str.lower()
+        al = df_municipios.query("codigo_uf==27")['nome'].str.lower()
+        se = df_municipios.query("codigo_uf==28")['nome'].str.lower()
+        ba = df_municipios.query("codigo_uf==29")['nome'].str.lower()
+        mg = df_municipios.query("codigo_uf==31")['nome'].str.lower()
+        es = df_municipios.query("codigo_uf==32")['nome'].str.lower()
+        rj = df_municipios.query("codigo_uf==33")['nome'].str.lower()
+        sp = df_municipios.query("codigo_uf==35")['nome'].str.lower()
+        pr = df_municipios.query("codigo_uf==41")['nome'].str.lower()
+        sc = df_municipios.query("codigo_uf==42")['nome'].str.lower()
+        rs = df_municipios.query("codigo_uf==43")['nome'].str.lower()
+        ms = df_municipios.query("codigo_uf==50")['nome'].str.lower()
+        mt = df_municipios.query("codigo_uf==51")['nome'].str.lower()
+        go = df_municipios.query("codigo_uf==52")['nome'].str.lower()
+        df1 = df_municipios.query("codigo_uf==53")['nome'].str.lower()
 
-            contador_de_cidades = {'ro': 0, 'ac': 0, 'am': 0, 'rr': 0, 'pa': 0, 'ap': 0, 'to': 0,
-                                   'ma': 0, 'pi': 0, 'ce': 0, 'rn': 0, 'pb': 0, 'pe': 0, 'al': 0,
-                                   'se': 0, 'ba': 0, 'mg': 0, 'es': 0, 'rj': 0, 'sp': 0, 'pr': 0,
-                                   'sc': 0, 'rs': 0, 'ms': 0, 'mt': 0, 'go': 0, 'df1': 0
-                                   }
+        contador_de_cidades = {'ro': 0, 'ac': 0, 'am': 0, 'rr': 0, 'pa': 0, 'ap': 0, 'to': 0,
+                               'ma': 0, 'pi': 0, 'ce': 0, 'rn': 0, 'pb': 0, 'pe': 0, 'al': 0,
+                               'se': 0, 'ba': 0, 'mg': 0, 'es': 0, 'rj': 0, 'sp': 0, 'pr': 0,
+                               'sc': 0, 'rs': 0, 'ms': 0, 'mt': 0, 'go': 0, 'df1': 0
+                               }
 
-            # criando uma lista para armazenar os dados que não forem encontrados
-            nao_encontradas = []
+        # criando uma lista para armazenar os dados que não forem encontrados
+        nao_encontradas = []
 
-            for linha in df_user_loc.values:
-                for cidade in linha.lower().split(','):
-                    if cidade.strip() in ro.values:
-                        contador_de_cidades['ro'] += 1
-                        break
-                    elif cidade.strip() in ac.values:
-                        contador_de_cidades['ac'] += 1
-                        break
-                    elif cidade.strip() in am.values:
-                        contador_de_cidades['am'] += 1
-                        break
-                    elif cidade.strip() in rr.values:
-                        contador_de_cidades['rr'] += 1
-                        break
-                    elif cidade.strip() in pa.values:
-                        contador_de_cidades['pa'] += 1
-                        break
-                    elif cidade.strip() in ap.values:
-                        contador_de_cidades['ap'] += 1
-                        break
-                    elif cidade.strip() in to.values:
-                        contador_de_cidades['to'] += 1
-                        break
-                    elif cidade.strip() in ma.values:
-                        contador_de_cidades['ma'] += 1
-                        break
-                    elif cidade.strip() in pi.values:
-                        contador_de_cidades['pi'] += 1
-                        break
-                    elif cidade.strip() in ce.values:
-                        contador_de_cidades['ce'] += 1
-                        break
-                    elif cidade.strip() in rn.values:
-                        contador_de_cidades['rn'] += 1
-                        break
-                    elif cidade.strip() in pb.values:
-                        contador_de_cidades['pb'] += 1
-                        break
-                    elif cidade.strip() in pe.values:
-                        contador_de_cidades['pe'] += 1
-                        break
-                    elif cidade.strip() in al.values:
-                        contador_de_cidades['al'] += 1
-                        break
-                    elif cidade.strip() in se.values:
-                        contador_de_cidades['se'] += 1
-                        break
-                    elif cidade.strip() in ba.values:
-                        contador_de_cidades['ba'] += 1
-                        break
-                    elif cidade.strip() in mg.values:
-                        contador_de_cidades['mg'] += 1
-                        break
-                    elif cidade.strip() in es.values:
-                        contador_de_cidades['es'] += 1
-                        break
-                    elif cidade.strip() in rj.values:
-                        contador_de_cidades['rj'] += 1
-                        break
-                    elif cidade.strip() in sp.values:
-                        contador_de_cidades['sp'] += 1
-                        break
-                    elif cidade.strip() in pr.values:
-                        contador_de_cidades['pr'] += 1
-                        break
-                    elif cidade.strip() in sc.values:
-                        contador_de_cidades['sc'] += 1
-                        break
-                    elif cidade.strip() in rs.values:
-                        contador_de_cidades['rs'] += 1
-                        break
-                    elif cidade.strip() in ms.values:
-                        contador_de_cidades['ms'] += 1
-                        break
-                    elif cidade.strip() in mt.values:
-                        contador_de_cidades['mt'] += 1
-                        break
-                    elif cidade.strip() in go.values:
-                        contador_de_cidades['go'] += 1
-                        break
-                    elif cidade.strip() in df1.values:
-                        contador_de_cidades['df1'] += 1
-                        break
-                    else:
-                        nao_encontradas.append(cidade.strip())
+        for linha in df_user_loc.values:
+            for cidade in linha.lower().split(','):
+                if cidade.strip() in ro.values:
+                    contador_de_cidades['ro'] += 1
+                    break
+                elif cidade.strip() in ac.values:
+                    contador_de_cidades['ac'] += 1
+                    break
+                elif cidade.strip() in am.values:
+                    contador_de_cidades['am'] += 1
+                    break
+                elif cidade.strip() in rr.values:
+                    contador_de_cidades['rr'] += 1
+                    break
+                elif cidade.strip() in pa.values:
+                    contador_de_cidades['pa'] += 1
+                    break
+                elif cidade.strip() in ap.values:
+                    contador_de_cidades['ap'] += 1
+                    break
+                elif cidade.strip() in to.values:
+                    contador_de_cidades['to'] += 1
+                    break
+                elif cidade.strip() in ma.values:
+                    contador_de_cidades['ma'] += 1
+                    break
+                elif cidade.strip() in pi.values:
+                    contador_de_cidades['pi'] += 1
+                    break
+                elif cidade.strip() in ce.values:
+                    contador_de_cidades['ce'] += 1
+                    break
+                elif cidade.strip() in rn.values:
+                    contador_de_cidades['rn'] += 1
+                    break
+                elif cidade.strip() in pb.values:
+                    contador_de_cidades['pb'] += 1
+                    break
+                elif cidade.strip() in pe.values:
+                    contador_de_cidades['pe'] += 1
+                    break
+                elif cidade.strip() in al.values:
+                    contador_de_cidades['al'] += 1
+                    break
+                elif cidade.strip() in se.values:
+                    contador_de_cidades['se'] += 1
+                    break
+                elif cidade.strip() in ba.values:
+                    contador_de_cidades['ba'] += 1
+                    break
+                elif cidade.strip() in mg.values:
+                    contador_de_cidades['mg'] += 1
+                    break
+                elif cidade.strip() in es.values:
+                    contador_de_cidades['es'] += 1
+                    break
+                elif cidade.strip() in rj.values:
+                    contador_de_cidades['rj'] += 1
+                    break
+                elif cidade.strip() in sp.values:
+                    contador_de_cidades['sp'] += 1
+                    break
+                elif cidade.strip() in pr.values:
+                    contador_de_cidades['pr'] += 1
+                    break
+                elif cidade.strip() in sc.values:
+                    contador_de_cidades['sc'] += 1
+                    break
+                elif cidade.strip() in rs.values:
+                    contador_de_cidades['rs'] += 1
+                    break
+                elif cidade.strip() in ms.values:
+                    contador_de_cidades['ms'] += 1
+                    break
+                elif cidade.strip() in mt.values:
+                    contador_de_cidades['mt'] += 1
+                    break
+                elif cidade.strip() in go.values:
+                    contador_de_cidades['go'] += 1
+                    break
+                elif cidade.strip() in df1.values:
+                    contador_de_cidades['df1'] += 1
+                    break
+                else:
+                    nao_encontradas.append(cidade.strip())
 
-            #Exibindo dataframe
-            loc_user = pd.Series(contador_de_cidades).sort_values(ascending=False)
-            st.dataframe(loc_user)
+        #Exibindo dataframe
+        loc_user = pd.Series(contador_de_cidades).sort_values(ascending=False)
+        st.dataframe(loc_user)
 
-            bt_mostra_graf_users = st.button("Exibir Gráfico")
-            if bt_mostra_graf_users:
-                grafico = pd.Series(data=contador_de_cidades).sort_values(ascending=False)
+        bt_mostra_graf_users = st.button("Exibir Gráfico")
+        if bt_mostra_graf_users:
+            grafico = pd.Series(data=contador_de_cidades).sort_values(ascending=False)
 
-                plt.figure(figsize=(16, 8))
-                sns.set_style("whitegrid")
-                plt.xticks(rotation=45)
+            plt.figure(figsize=(16, 8))
+            sns.set_style("whitegrid")
+            plt.xticks(rotation=45)
 
-                sns.barplot(x=grafico.index.str.upper(),
-                            y=grafico.values,
-                            palette=sns.color_palette("BuGn_r", n_colors=len(grafico.index))).set_title("CIDADES")
-                st.pyplot()
+            sns.barplot(x=grafico.index.str.upper(),
+                        y=grafico.values,
+                        palette=sns.color_palette("BuGn_r", n_colors=len(grafico.index))).set_title("CIDADES")
+            st.pyplot()
 
-        df_download = df[['source', 'target', 'url']]
-        st.dataframe(df_download.head())
+        df_download = df[['source', 'target', 'retweet_count', 'url']]
 
-        st.header('Faça o download do .csv: ')
+        st.markdown("___")
+
+        st.header('Faça o download do **.csv**: ')
         st.subheader("Esse arquivo está pronto para ser importado pelo Gephi!")
+        st.markdown("Não se esqueça de renomear e colocar a extensão `.csv`")
         st.markdown(get_table_download_link(df_download), unsafe_allow_html=True)
 
-
-
-
+        st.header("")
+        st.markdown("[![paypal](https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=miguel.s.machado@hotmail.com)")
+        st.markdown("Ajuda a manter essa iniciativa **Open Soure** funcionando!")
+        st.markdown("Colabore com a comunidade")
 
 
 if __name__ == '__main__':
